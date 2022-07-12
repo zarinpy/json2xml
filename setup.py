271c2050ec7ef63344c5e190e1d2e7406908aed0
omid <zarinpy@192.168.1.100>
2022-07-13 02:48:14 +0300

    create setup.cfg

--- setup.py
+++ setup.py
@@ -1,19 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='json2xml',
-    version='0.1',
-    packages=find_packages(),
-    include_package_data=True,
-    license='MIT',
-    description='convert json to xml',
-    long_description=open('README.md').read(),
-    url='',
-    author='zarinpy',
-    author_email='',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8',
-    ],
-)
+from setuptools import setup
+setup()
