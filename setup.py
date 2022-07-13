3be904abd9d4c519d602e9935f305d73546e3206
omid <zarinpy@192.168.1.100>
2022-07-13 14:13:25 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.9',
     ],
     py_modules=['json2xml.src:Json2Xml'],
 )
